9bc2f0bbfd15bf95da7099982eee0a5514ceec9f
Matthew Messinger <mattmess1221@gmail.com>
2020-05-04 22:58:08 -0400

    forge: Fix wrong import for I18n

--- forge/src/main/java/com/minelittlepony/hdskins/forge/client/gui/MCPButton.java
+++ forge/src/main/java/com/minelittlepony/hdskins/forge/client/gui/MCPButton.java
@@ -1,10 +1,10 @@
 package com.minelittlepony.hdskins.forge.client.gui;
 
 import com.minelittlepony.hdskins.common.gui.IButton;
-import com.sun.org.apache.xml.internal.security.utils.I18n;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.widget.button.AbstractButton;
 
+import net.minecraft.client.resources.I18n;
 import javax.annotation.Nullable;
 import java.util.function.Consumer;
 
@@ -15,9 +15,9 @@ public class MCPButton extends AbstractButton implements IButton {
     private final Consumer<IButton> callback;
 
     public MCPButton(int i, int j, int k, int l, String string, @Nullable String tooltip, Consumer<IButton> callback) {
-        super(i, j, k, l, I18n.translate(string));
+        super(i, j, k, l, I18n.format(string));
         if (tooltip != null) {
-            this.tooltip = I18n.translate(tooltip);
+            this.tooltip = I18n.format(tooltip);
         } else {
             this.tooltip = null;
         }
